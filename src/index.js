65555f8e996560ee379291a85367689a85e1ee9d
Jadgptw <Gordin.Alexander90@gmail.com>
2019-01-24 23:04:43 +0300

    feat: add navigation and required pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { Link, HashRouter, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from './components/Home';
+import Member from './components/Member';
+import Search from './components/Search';
+
+ReactDOM.render(
+    <HashRouter>
+      <Fragment>
+        <nav className="main-navigation">
+          <Link to="/">Home Page </Link>
+          <Link to="search"> Search Page </Link>
+          <Link to="member"> Member Page </Link>
+        </nav>
+
+        <main className="container">
+          <Route exact path="/" component={Home}/>
+          <Route path="/member" component={Member}/>
+          <Route path="/search" component={Search}/>
+        </main>
+      </Fragment>
+    </HashRouter>,
+  document.getElementById('root'));
