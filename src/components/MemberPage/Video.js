a06e4c115b0a41e3869974fb8800d18031e08f53
AndVikVin <vintskevich@gmail.com>
2019-01-26 14:57:26 +0200

    feat: Add overlay to Video component

--- src/components/MemberPage/Video.js
+++ src/components/MemberPage/Video.js
@@ -1,31 +1,29 @@
 import React, { Component } from 'react';
-import YouTube from 'react-youtube'
+import '../node_modules/react-modal-video/scss/modal-video.scss';
+import ModalVideo from 'react-modal-video'
 
 class Video extends Component{
-    render() {
-        //this.props.video it's url of a youtube video
+    constructor (props) {
+        super(props)
+        this.state = {
+          isOpen: false
+        }
+      }
+    
+      render(){
+        const openModal = () => {
+            this.setState({isOpen: true})
+        }
+        // this.props.video it's url of a youtube video
         const url = this.props.video;
         const videoId = url.substring(url.indexOf('=')+1);
         console.log(videoId);
-        const opts = {
-          height: '390',
-          width: '640',
-          playerVars: {
-            autoplay: 1
-          }
-        };
-        return (
-          <YouTube
-            id='video' 
-            videoId = {videoId}
-            opts={opts}
-            onReady={this._onReady}
-          />
-        );
+        return(
+            <div>
+                <ModalVideo channel='youtube' isOpen={this.state.isOpen} videoId={videoId} onClose={() => this.setState({isOpen: false})} />
+                <button onClick={openModal}>Open</button>
+            </div>
+        ) 
       }
-     
-      _onReady(event) {
-        event.target.pauseVideo();
-      }
-    }
+}
 export default Video;
