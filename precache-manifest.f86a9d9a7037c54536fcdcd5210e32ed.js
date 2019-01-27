ae8541720370697ab5ff0c48ebca307b09277764
Jadgptw <Gordin.Alexander90@gmail.com>
2019-01-27 22:55:54 +0300

    Updates

---
+++ precache-manifest.f86a9d9a7037c54536fcdcd5210e32ed.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "5de87cf84309be269c556086febc585c",
+    "url": "/RSS-CodeJam-5/static/media/ava1.5de87cf8.jpg"
+  },
+  {
+    "revision": "498cf9dcfc1a75af92dd",
+    "url": "/RSS-CodeJam-5/static/css/main.224b16e3.chunk.css"
+  },
+  {
+    "revision": "a071bb78f64e629abde6db40ac7ed8cd",
+    "url": "/RSS-CodeJam-5/static/media/ava5.a071bb78.jpg"
+  },
+  {
+    "revision": "c3fb5d39fb3b1df4c97a",
+    "url": "/RSS-CodeJam-5/static/js/1.c3fb5d39.chunk.js"
+  },
+  {
+    "revision": "e186c0985f089c056480",
+    "url": "/RSS-CodeJam-5/static/js/runtime~main.e186c098.js"
+  },
+  {
+    "revision": "152e80e8a21b1a749ed993fe05c41686",
+    "url": "/RSS-CodeJam-5/static/media/homePageBG.152e80e8.jpg"
+  },
+  {
+    "revision": "498cf9dcfc1a75af92dd",
+    "url": "/RSS-CodeJam-5/static/js/main.498cf9dc.chunk.js"
+  },
+  {
+    "revision": "c5187f4975b463ae971efba34daf3dc6",
+    "url": "/RSS-CodeJam-5/static/media/ava2.c5187f49.jpg"
+  },
+  {
+    "revision": "2571cba16b1187426280386b72a8c621",
+    "url": "/RSS-CodeJam-5/static/media/ava3.2571cba1.jpg"
+  },
+  {
+    "revision": "3d2a7a9ea4b0565416fce0d83381fc85",
+    "url": "/RSS-CodeJam-5/static/media/ava4.3d2a7a9e.jpg"
+  },
+  {
+    "revision": "c3fb5d39fb3b1df4c97a",
+    "url": "/RSS-CodeJam-5/static/css/1.10488b73.chunk.css"
+  },
+  {
+    "revision": "956d4a0106cddfce39a40f7c4c99439e",
+    "url": "/RSS-CodeJam-5/index.html"
+  }
+];
